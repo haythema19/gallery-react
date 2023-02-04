3c97a7e743c15b607a70e223932f19a344ddb9be
haythema19 <duongpc99@gmail.com>
2023-02-04 23:18:30 +0700

    Update UserProfile.jsx

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -133,7 +133,7 @@ const UserProfile = () => {
 
         {pins?.length === 0 && (
           <div className="flex justify-center font-bold items-center w-full text-1xl mt-2">
-            No Pins Found!
+            Chưa có ảnh nào.
           </div>
         )}
       </div>
